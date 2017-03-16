8ff938a8cc3eaeb305166b2a5922190e5b1206e2
Kevin <kevin@rocher-dev.com>
2017-03-16 12:43:26 +0100

    v 0.1.0
    * Simple DB Backup.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # dbBackup
 Backup a MySQL database on a shared hosting and keep an history
+
+## Config file
+
+Create a file named backup-config.sh` in the repository.
+
+```#/bin/bash
+MYSQL_HOST='';
+MYSQL_USER='';
+MYSQL_PASS='';
+MYSQL_BASE='';
+BACKUP_FOLDER='/absolute/path/to/backupfolder';
+BACKUP_DAYS=5;
+```
---
+++ backup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+echo '################';
+echo '# DB Backup';
+echo '# v 0.1.0';
+echo '# By @Darklg';
+echo '################';
+echo '';
+
+###################################
+## Basic vars
+###################################
+
+# Get absolute path to the script
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/"
+
+###################################
+## Config
+###################################
+
+CONFIG_FILE="${SCRIPT_PATH}/backup-config.sh";
+if [[ ! -f "${CONFIG_FILE}" ]];then
+    echo "# You must have a config file !";
+    return 0;
+else
+    echo "# Loading user config";
+    /bin/bash "${CONFIG_FILE}";
+fi;
+
+###################################
+## Install & Clean
+###################################
+
+if [[ ! -d "${BACKUP_FOLDER}" ]];then
+    echo "# Creating backup folder";
+    mkdir "${BACKUP_FOLDER}";
+fi;
+
+echo "# Deleting backups older than ${BACKUP_DAYS} days.";
+find "${BACKUP_FOLDER}"* -mtime +"${BACKUP_DAYS}" -exec rm {} \;
+
+###################################
+## Backup
+###################################
+
+BACKUP_FILE="${BACKUP_FOLDER}/backup-${MYSQL_BASE}-$(date +"%Y%m%d-%H%M%S").sql";
+
+echo "# Backup database";
+mysqldump -h "${MYSQL_HOST}" -u "${MYSQL_USER}" -p"${MYSQL_PASS}" "${MYSQL_BASE}" > "${BACKUP_FILE}";
+
+echo "# Compress backup";
+gzip "${BACKUP_FILE}";
+
+###################################
+## Good to go
+###################################
+
+echo "# Backup is over !";
